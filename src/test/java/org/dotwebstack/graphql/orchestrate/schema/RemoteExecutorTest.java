6dd39fa8772e019be22b2fdea328f6c8330acae1
Cor <60174327+kad-switzc@users.noreply.github.com>
2024-01-26 15:55:12 +0100

    [DHUB-3386] Add custom exception for errors (#148)
    Added a custom exception to use when a 4xx or 5xx error occurs. This instead of the deafault WebClientResponseException.

--- src/test/java/org/dotwebstack/graphql/orchestrate/schema/RemoteExecutorTest.java
+++ src/test/java/org/dotwebstack/graphql/orchestrate/schema/RemoteExecutorTest.java
@@ -2,11 +2,13 @@ package org.dotwebstack.graphql.orchestrate.schema;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import graphql.ExceptionWhileDataFetching;
 import graphql.ExecutionInput;
 import java.io.IOException;
 import java.net.URI;
@@ -14,10 +16,12 @@ import java.util.Map;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
+import org.dotwebstack.graphql.orchestrate.exception.GraphqlJavaOrchestrateException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
@@ -50,10 +54,7 @@ class RemoteExecutorTest {
 
   @Test
   void execute_performsHttpCall_OnGivenEndpoint() throws Exception {
-    var executor = RemoteExecutor.newExecutor()
-        .endpoint(URI.create(String.format("http://%s:%d", mockWebServer.getHostName(), mockWebServer.getPort())))
-        .webClient(webClient)
-        .build();
+    var executor = getRemoteExecutor();
 
     var query = "{brewery(identifier:\"foo\") {identifier name}}";
 
@@ -82,10 +83,7 @@ class RemoteExecutorTest {
 
   @Test
   void execute_passesThroughVars_WhenVarsPresent() throws Exception {
-    var executor = RemoteExecutor.newExecutor()
-        .endpoint(URI.create(String.format("http://%s:%d", mockWebServer.getHostName(), mockWebServer.getPort())))
-        .webClient(webClient)
-        .build();
+    var executor = getRemoteExecutor();
 
     var query = "query Query($identifier: String) {brewery($identifier) {identifier name}}";
     Map<String, Object> variables = Map.of("identifier", "foo");
@@ -112,6 +110,85 @@ class RemoteExecutorTest {
     assertThat(request.getHeader(HttpHeaders.ACCEPT), equalTo(MediaType.APPLICATION_JSON_VALUE));
     assertThat(requestBody.get("query"), equalTo(query));
     assertThat(requestBody.get("variables"), equalTo(variables));
+  }
+
+  @Test
+  void execute_passesThroughExceptionDetails_WhenExceptionDetailsPresent() throws Exception {
+    var executor = getRemoteExecutor();
+
+    var query = "query Query($identifier: String) {brewery($identifier) {identifier name}}";
+    Map<String, Object> variables = Map.of("identifier", "foo");
+
+    var input = ExecutionInput.newExecutionInput()
+        .query(query)
+        .variables(variables)
+        .build();
+
+    mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .setBody(OBJECT_MAPPER.writeValueAsString(Map.of("status", "400", "detail", "Bad request"))));
+
+    var result = executor.execute(input)
+        .get();
+
+    assertThat(result, notNullValue());
+    assertThat(result.getErrors()
+        .size(), is(1));
+    var error = result.getErrors()
+        .get(0);
+
+    assertThat(error.getClass(), is(ExceptionWhileDataFetching.class));
+    var exception = ((ExceptionWhileDataFetching) error).getException();
+
+    assertThat(exception.getClass(), is(GraphqlJavaOrchestrateException.class));
+    var graphqlException = (GraphqlJavaOrchestrateException) exception;
+
+    assertThat(graphqlException.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+    assertThat(graphqlException.getMessage(), is("Bad request"));
+
+    mockWebServer.takeRequest();
+  }
+
+  @Test
+  void execute_passesThroughInternalServerError_WhenUnknownBodyDetails() throws Exception {
+    var executor = getRemoteExecutor();
+
+    var query = "query Query($identifier: String) {brewery($identifier) {identifier name}}";
+    Map<String, Object> variables = Map.of("identifier", "foo");
+
+    var input = ExecutionInput.newExecutionInput()
+        .query(query)
+        .variables(variables)
+        .build();
+
+    mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .setBody(OBJECT_MAPPER.writeValueAsString(Map.of("unknown", "unknown"))));
+
+    var result = executor.execute(input)
+        .get();
+
+    assertThat(result, notNullValue());
+    assertThat(result.getErrors()
+        .size(), is(1));
+    var error = result.getErrors()
+        .get(0);
+
+    assertThat(error.getClass(), is(ExceptionWhileDataFetching.class));
+    var exception = ((ExceptionWhileDataFetching) error).getException();
+
+    assertThat(exception.getClass(), is(GraphqlJavaOrchestrateException.class));
+    var graphqlException = (GraphqlJavaOrchestrateException) exception;
+
+    assertThat(graphqlException.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+    assertThat(graphqlException.getMessage(), is("Something went wrong while orchestrating the request."));
+
+    mockWebServer.takeRequest();
+  }
+
+  private static RemoteExecutor getRemoteExecutor() {
+    return RemoteExecutor.newExecutor()
+        .endpoint(URI.create(String.format("http://%s:%d", mockWebServer.getHostName(), mockWebServer.getPort())))
+        .webClient(webClient)
+        .build();
   }
 
   private Map<String, Object> getRequestBody(RecordedRequest request) throws JsonProcessingException {
